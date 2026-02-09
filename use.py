bad83e4e4887a6bd5f547dd96eb0abd9458c6715
vargab100 <121413207+vargab100@users.noreply.github.com>
2026-02-09 11:58:14 +0530

    Implement model scoring and JSON conversion functions

---
+++ use.py
@@ -0,0 +1,18 @@
+import os
+import requests
+import numpy as np
+import pandas as pd
+import json
+
+def create_tf_serving_json(data):
+    return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
+
+def score_model(dataset):
+    url = 'https://dbc-84d873a5-607b.cloud.databricks.com/serving-endpoints/demo/invocations'
+    headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}', 'Content-Type': 'application/json'}
+    ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
+    data_json = json.dumps(ds_dict, allow_nan=True)
+    response = requests.request(method='POST', headers=headers, url=url, data=data_json)
+    if response.status_code != 200:
+        raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+    return response.json()
